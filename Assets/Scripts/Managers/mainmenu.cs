35592eb8d3d168e96b31f2ca40e772cbe7b5a24e
LuciaMeyerArana1 <90474380+LuciaMeyerArana1@users.noreply.github.com>
2022-06-12 18:06:57 -0300

    sadasdad

--- Assets/Scripts/Managers/mainmenu.cs
+++ Assets/Scripts/Managers/mainmenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+        SceneManager.LoadScene("The Real deal"); 
     }
     public void QuitGame()
     {
