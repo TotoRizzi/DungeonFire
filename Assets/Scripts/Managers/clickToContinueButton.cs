ace2b1611ff4fe4a2bb17e94d251cdcd08eaa27e
TotoRizzi <83927597+TotoRizzi@users.noreply.github.com>
2022-06-23 16:30:15 -0300

    <a
    asdasd

--- Assets/Scripts/Managers/clickToContinueButton.cs
+++ Assets/Scripts/Managers/clickToContinueButton.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class clickToContinueButton : MonoBehaviour
+public class ClickToContinueButton : MonoBehaviour
 {
-    
-
-
+    public void DestroyGameObject()
+    {
+        Destroy(this.gameObject);
+    }
     public void OnClick()
     {
-        GameManager.instance.GoToMenu();
+        Debug.Log("Funciono");
+        SceneManager.LoadScene("Main Menu");
     }
-   
 }
